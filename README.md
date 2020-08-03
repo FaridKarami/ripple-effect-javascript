c15ecd1190be608790fe7df66c1efba2deca15ec
faridkarami <faridkarami71@gmail.com>
2020-08-03 15:33:08 +0430

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+------------
+About Author
+------------
+
+This project is to create a ripple effect to apply all the HTML elements
+
+#####usage
+
+    Assign the fk8-ripple attribute to any HTML tag you want
+
+#####disabled class
+
+    This effect does not work on HTML tags that have a disabled class
+
+-------------------------
+Design Decisions & Issues
+-------------------------
+
+No libraries or plugins have been used to design this project and it has been designed and implemented natively with JavaScript and CSS.
+
+----------
+Contact Us
+----------
+#####Personal Website: [FK8.IR](https://fk8.ir)
+#####Company Website: [GitNet](https://gitnet.ir)
---
+++ source/script.js
@@ -0,0 +1,45 @@
+window.onload = () => {
+    const selector = '[fk8-ripple]';
+    const fk8Ripple = document.querySelectorAll(selector);
+
+    for (const item of fk8Ripple) {
+        const self = item;
+        item.addEventListener('mousedown', function(e) {
+
+            if(self.classList.contains('disabled')) {
+                return;
+            }
+            if(self.closest(selector)) {
+                e.stopPropagation();
+            }
+
+            const style = getComputedStyle(self);
+
+            const initPos = style.position;
+            const x = e.pageX - self.offsetLeft;
+            const y = e.pageY - self.offsetTop;
+            const dia = Math.min(this.offsetHeight, this.offsetWidth, 100);
+
+            // create ripple and append
+            const rippleAnimate = document.createElement('div');
+            rippleAnimate.classList.add('ripple');
+            self.append(rippleAnimate);
+
+            if(!initPos || initPos === "static") {
+                self.style.position = 'relative';
+            }
+
+            rippleAnimate.classList.add('rippleWave');
+            rippleAnimate.setAttribute('style', [
+                `width: ${dia}px`,
+                `height: ${dia}px`,
+                `left: ${x - (dia/2)}px`,
+                `top: ${y - (dia/2)}px`,
+            ].join(';'));
+
+            rippleAnimate.addEventListener("animationend", function() {
+                rippleAnimate.remove();
+            }, false);
+        })
+    }
+}
